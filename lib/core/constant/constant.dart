f0a56ffc3f2b60f8f23750abea2f89b5f38b78c1
abdullah017 <abdullahozvanli@gmail.com>
2024-10-13 02:07:12 +0300

    Add [Home Page AppBar Widget - Constant Value] Fix [Home Page Widget and Method]

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -1,4 +1,15 @@
-// lib/core/constants.dart
 const String graphqlEndpoint = 'https://vortexsura.softvortex.com/v1/graphql';
 const String hasuraAdminSecret = 'myadminsecretkey';
-const String webSocketLink =  'wss://vortexsura.softvortex.com/v1/graphql';
+const String webSocketLink = 'wss://vortexsura.softvortex.com/v1/graphql';
+
+const String darkLogo = 'assets/abdullahtas_dark.png';
+const String lightLogo = 'assets/abdullahtas_LOGO.png';
+
+const String notFoundArticle = 'Hiç yazı bulunamadı';
+const String searchArticle = 'Makale ara...';
+
+const String animatedFlutter = 'Flutter...';
+const String animatedDart = 'Dart...';
+const String animatedHasura = 'Hasura...';
+const String animatedGraphQL = 'GraphQL...';
+const String animatedFirebase = 'Firebase...';
