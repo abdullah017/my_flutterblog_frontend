c4f20e7395224fc07631d95681261c7f21cb714e
abdullah017 <abdullahozvanli@gmail.com>
2024-10-13 02:31:28 +0300

    Fix[ScrollView]

--- lib/presentation/pages/post/post_detail_page.dart
+++ lib/presentation/pages/post/post_detail_page.dart
@@ -7,12 +7,32 @@ import 'package:my_blogapp_frontend/core/helper/text_capitalize_helper.dart';
 import 'package:my_blogapp_frontend/domain/entities/post_entites.dart';
 import '../../controllers/post_controller/post_controller.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class PostDetailPage extends StatelessWidget {
+class PostDetailPage extends StatefulWidget {
   final int postId;
   final String title;
 
   const PostDetailPage({super.key, required this.postId, required this.title});
+
+  @override
+  _PostDetailPageState createState() => _PostDetailPageState();
+}
+
+class _PostDetailPageState extends State<PostDetailPage> {
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +43,8 @@ class PostDetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          capitalize(title), //Slug için değişen başlık kısmını tekrar düzenleme
+          capitalize(widget
+              .title), // Slug için değişen başlık kısmını tekrar düzenleme
           style: theme.textTheme.titleLarge?.copyWith(
             color: theme.appBarTheme.titleTextStyle?.color,
           ),
@@ -35,7 +56,7 @@ class PostDetailPage extends StatelessWidget {
       ),
       body: FutureBuilder<Post?>(
         future: controller
-            .getPostById(postId)
+            .getPostById(widget.postId)
             .then((either) => either.getOrElse(() => null)),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -62,79 +83,102 @@ class PostDetailPage extends StatelessWidget {
             ));
           }
 
-          return SingleChildScrollView(
-            padding: const EdgeInsets.all(16.0),
-            child: Center(
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(maxWidth: 800),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Kapak Görseli
-                    if (post.coverImage != null && post.coverImage!.isNotEmpty)
-                      CachedNetworkImage(
-                        imageUrl: post.coverImage!,
-                        placeholder: (context, url) => const SizedBox(
+          return Scrollbar(
+            controller: _scrollController,
+            thumbVisibility: true, // Scrollbar'ı her zaman görünür yapar
+            thickness: 8.0, // Scrollbar kalınlığı
+            radius: const Radius.circular(4), // Scrollbar köşe yuvarlaklığı
+            child: SingleChildScrollView(
+              controller: _scrollController,
+              padding: const EdgeInsets.all(16.0),
+              child: Center(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 800),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Kapak Görseli
+                      if (post.coverImage != null &&
+                          post.coverImage!.isNotEmpty)
+                        CachedNetworkImage(
+                          imageUrl: post.coverImage!,
+                          placeholder: (context, url) => const SizedBox(
+                            height: 250,
+                            child: Center(child: CircularProgressIndicator()),
+                          ),
+                          errorWidget: (context, url, error) => Container(
+                            height: 250,
+                            color: Colors.grey[200],
+                            child: const Icon(Icons.broken_image,
+                                size: 100, color: Colors.grey),
+                          ),
+                          width: double.infinity,
                           height: 250,
-                          child: Center(child: CircularProgressIndicator()),
+                          fit: BoxFit.cover,
+                        )
+                      else
+                        // Varsayılan bir kapak resmi göster
+                        ClipRRect(
+                          borderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(15)),
+                          child: Image.asset(
+                            'assets/images/default_cover.png', // Projenize uygun varsayılan resim ekleyin
+                            height: 250,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                        errorWidget: (context, url, error) => Container(
-                          height: 250,
-                          color: Colors.grey[200],
-                          child: const Icon(Icons.broken_image,
-                              size: 100, color: Colors.grey),
+                      const SizedBox(height: 16),
+
+                      // Başlık
+                      SelectableText(
+                        post.title,
+                        style: theme.textTheme.headlineSmall?.copyWith(
+                          fontWeight: FontWeight.bold,
                         ),
-                        width: double.infinity,
-                        height: 250,
-                        fit: BoxFit.cover,
                       ),
-                    const SizedBox(height: 16),
+                      const SizedBox(height: 8),
 
-                    // Başlık
-                    SelectableText(
-                      post.title,
-                      style: theme.textTheme.headlineSmall?.copyWith(
-                        fontWeight: FontWeight.bold,
+                      // Oluşturulma Tarihi
+                      Text(
+                        'Oluşturulma Tarihi: ${post.createdAt.toLocal().toString().split(' ')[0]}',
+                        style: theme.textTheme.bodySmall?.copyWith(
+                          color: isDarkMode
+                              ? Colors.white70
+                              : Colors.grey.shade600,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 8),
+                      const SizedBox(height: 24),
 
-                    // Oluşturulma Tarihi
-                    Text(
-                      'Oluşturulma Tarihi: ${post.createdAt.toLocal().toString().split(' ')[0]}',
-                      style: theme.textTheme.bodySmall?.copyWith(
-                        color:
-                            isDarkMode ? Colors.white70 : Colors.grey.shade600,
+                      // İçerik
+                      SelectableLinkify(
+                        onOpen: (link) async {
+                          final Uri uri = Uri.parse(link.url);
+                          if (await canLaunchUrl(uri)) {
+                            await launchUrl(uri,
+                                mode: LaunchMode.externalApplication);
+                          } else {
+                            Get.snackbar('Hata', 'URL açılamadı.');
+                          }
+                        },
+                        text: post.content,
+                        style: GoogleFonts.openSans(
+                          textStyle: theme.textTheme.bodyLarge?.copyWith(
+                            fontSize: 18.0,
+                            height: 1.6,
+                            color: theme.textTheme.bodyLarge?.color,
+                          ),
+                        ),
+                        linkStyle: TextStyle(
+                          color: theme.colorScheme.primary,
+                          decoration: TextDecoration.underline,
+                        ),
+                        options: const LinkifyOptions(looseUrl: true),
+                        textAlign: TextAlign.left,
+                        maxLines: null,
                       ),
-                    ),
-                    const SizedBox(height: 24),
-
-                    // İçerik
-                    SelectableLinkify(
-                      onOpen: (link) async {
-                        final Uri uri = Uri.parse(link.url);
-                        if (await canLaunchUrl(uri)) {
-                          await launchUrl(uri,
-                              mode: LaunchMode.externalApplication);
-                        } else {
-                          Get.snackbar('Hata', 'URL açılamadı.');
-                        }
-                      },
-                      text: post.content,
-                      style: theme.textTheme.bodyLarge?.copyWith(
-                        fontSize: 18.0,
-                        height: 1.6,
-                        color: theme.textTheme.bodyLarge?.color,
-                      ),
-                      linkStyle: TextStyle(
-                        color: theme.colorScheme.primary,
-                        decoration: TextDecoration.underline,
-                      ),
-                      options: const LinkifyOptions(looseUrl: true),
-                      textAlign: TextAlign.left,
-                      maxLines: null,
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
