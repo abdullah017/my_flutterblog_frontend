fd3130b7685bd06f911768aad0a06fb93b702ed6
abdullah017 <abdullahozvanli@gmail.com>
2024-10-15 17:16:14 +0300

    Fix[Code improvements in PostController and PostDetail pages]

--- lib/presentation/controllers/post_controller/post_controller.dart
+++ lib/presentation/controllers/post_controller/post_controller.dart
@@ -1,10 +1,10 @@
-// lib/presentation/controllers/post_controller.dart
 import 'package:get/get.dart';
 import 'package:dartz/dartz.dart';
 import 'package:my_blogapp_frontend/core/errors/failures.dart';
 import 'package:my_blogapp_frontend/domain/entities/post_entites.dart';
 import 'package:my_blogapp_frontend/domain/usecases/get_post_by_id_usecase.dart';
 import 'package:my_blogapp_frontend/domain/usecases/get_post_usecase.dart';
+import 'package:flutter/material.dart';
 
 class PostController extends GetxController {
   final GetPosts getPosts;
@@ -15,8 +15,6 @@ class PostController extends GetxController {
     required this.getPostById,
   });
 
-  /// These lines of code are defining reactive variables using GetX library in Dart. Here's what each
-  /// variable is doing:
   // All posts fetched from the backend
   var allPosts = <Post>[].obs;
 
@@ -30,16 +28,26 @@ class PostController extends GetxController {
   var errorMessage = ''.obs;
   var selectedPost = Rxn<Post>();
 
+  // ScrollController for the PostDetailPage
+  late ScrollController scrollController;
+
   @override
   void onInit() {
     super.onInit();
     fetchPosts();
-    //Search
+    // Initialize ScrollController
+    scrollController = ScrollController();
+    // Search
     everAll([allPosts, searchTerm], (_) => filterPosts());
   }
 
-  /// The `fetchPosts` function fetches posts asynchronously, updates state based on the result, and
-  /// handles potential errors.
+  @override
+  void onClose() {
+    scrollController.dispose();
+    super.onClose();
+  }
+
+  /// Fetches all posts from the backend.
   Future<void> fetchPosts() async {
     isLoading.value = true;
     final Either<Failure, List<Post>> result = await getPosts.call();
@@ -54,11 +62,7 @@ class PostController extends GetxController {
     isLoading.value = false;
   }
 
-  /// The function `filterPosts` filters a list of posts based on a search term provided by the user.
-  ///
-  /// Returns:
-  ///   The `filterPosts()` function returns a list of posts that contain the search term in their title.
-  /// If the search term is empty, it returns all posts.
+  /// Filters posts based on the search term.
   void filterPosts() {
     if (searchTerm.value.isEmpty) {
       filteredPosts.value = allPosts;
@@ -70,13 +74,7 @@ class PostController extends GetxController {
     }
   }
 
-  /// This function fetches a post by its ID, updates the UI with the fetched post or error message, and
-  /// manages a loading state.
-  ///
-  /// Args:
-  ///   id (int): The `id` parameter in the `fetchPostById` function represents the unique identifier of
-  /// the post that you want to fetch. This function is responsible for fetching a post by its ID
-  /// asynchronously.
+  /// Fetches a post by its ID.
   Future<void> fetchPostById(int id) async {
     isLoading.value = true;
     final Either<Failure, Post?> result = await getPostById.call(id);
--- lib/presentation/pages/post/post_detail_page.dart
+++ lib/presentation/pages/post/post_detail_page.dart
@@ -1,4 +1,3 @@
-// lib/presentation/pages/post_detail_page.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:get/get.dart';
@@ -9,30 +8,11 @@ import '../../controllers/post_controller/post_controller.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class PostDetailPage extends StatefulWidget {
+class PostDetailPage extends StatelessWidget {
   final int postId;
   final String title;
 
   const PostDetailPage({super.key, required this.postId, required this.title});
-
-  @override
-  _PostDetailPageState createState() => _PostDetailPageState();
-}
-
-class _PostDetailPageState extends State<PostDetailPage> {
-  late ScrollController _scrollController;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController = ScrollController();
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,11 +20,13 @@ class _PostDetailPageState extends State<PostDetailPage> {
     final theme = Theme.of(context);
     final isDarkMode = theme.brightness == Brightness.dark;
 
+    // Fetch the post details when the page is built
+    controller.fetchPostById(postId);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          capitalize(widget
-              .title), // Slug için değişen başlık kısmını tekrar düzenleme
+          capitalize(title),
           style: theme.textTheme.titleLarge?.copyWith(
             color: theme.appBarTheme.titleTextStyle?.color,
           ),
@@ -54,137 +36,124 @@ class _PostDetailPageState extends State<PostDetailPage> {
         iconTheme: theme.appBarTheme.iconTheme,
         centerTitle: true,
       ),
-      body: FutureBuilder<Post?>(
-        future: controller
-            .getPostById(widget.postId)
-            .then((either) => either.getOrElse(() => null)),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-          if (snapshot.hasError) {
-            return Center(
-                child: Text(
-              'Hata: ${snapshot.error}',
-              style: theme.textTheme.bodyLarge,
-            ));
-          }
+        if (controller.errorMessage.value.isNotEmpty) {
+          return Center(
+              child: Text(
+            'Hata: ${controller.errorMessage.value}',
+            style: theme.textTheme.bodyLarge,
+          ));
+        }
 
-          final Post? post = snapshot.data;
+        final Post? post = controller.selectedPost.value;
 
-          if (post == null) {
-            return Center(
-                child: Text(
-              'Yazı bulunamadı.',
-              style: theme.textTheme.bodyLarge?.copyWith(
-                fontSize: 18,
-              ),
-            ));
-          }
+        if (post == null) {
+          return Center(
+              child: Text(
+            'Yazı bulunamadı.',
+            style: theme.textTheme.bodyLarge?.copyWith(
+              fontSize: 18,
+            ),
+          ));
+        }
 
-          return Scrollbar(
-            controller: _scrollController,
-            thumbVisibility: true, // Scrollbar'ı her zaman görünür yapar
-            thickness: 8.0, // Scrollbar kalınlığı
-            radius: const Radius.circular(4), // Scrollbar köşe yuvarlaklığı
-            child: SingleChildScrollView(
-              controller: _scrollController,
-              padding: const EdgeInsets.all(16.0),
-              child: Center(
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 800),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Kapak Görseli
-                      if (post.coverImage != null &&
-                          post.coverImage!.isNotEmpty)
-                        CachedNetworkImage(
-                          imageUrl: post.coverImage!,
-                          placeholder: (context, url) => const SizedBox(
-                            height: 250,
-                            child: Center(child: CircularProgressIndicator()),
-                          ),
-                          errorWidget: (context, url, error) => Container(
-                            height: 250,
-                            color: Colors.grey[200],
-                            child: const Icon(Icons.broken_image,
-                                size: 100, color: Colors.grey),
-                          ),
-                          width: double.infinity,
+        return Scrollbar(
+          controller: controller.scrollController,
+          thumbVisibility: true, // Scrollbar'ı her zaman görünür yapar
+          thickness: 8.0, // Scrollbar kalınlığı
+          radius: const Radius.circular(4), // Scrollbar köşe yuvarlaklığı
+          child: SingleChildScrollView(
+            controller: controller.scrollController,
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 800),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Kapak Görseli
+                    if (post.coverImage != null && post.coverImage!.isNotEmpty)
+                      CachedNetworkImage(
+                        imageUrl: post.coverImage!,
+                        placeholder: (context, url) => const SizedBox(
                           height: 250,
-                          fit: BoxFit.cover,
-                        )
-                      else
-                        // Varsayılan bir kapak resmi göster
-                        ClipRRect(
-                          borderRadius: const BorderRadius.vertical(
+                          child: Center(child: CircularProgressIndicator()),
+                        ),
+                        errorWidget: (context, url, error) => Container(
+                          height: 250,
+                          color: Colors.grey[200],
+                          child: const Icon(Icons.broken_image,
+                              size: 100, color: Colors.grey),
+                        ),
+                        width: double.infinity,
+                        height: 250,
+                        fit: BoxFit.cover,
+                      )
+                    else
+                      // Varsayılan bir kapak resmi göster
+                      const ClipRRect(
+                          borderRadius:  BorderRadius.vertical(
                               top: Radius.circular(15)),
-                          child: Image.asset(
-                            'assets/images/default_cover.png', // Projenize uygun varsayılan resim ekleyin
-                            height: 250,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      const SizedBox(height: 16),
+                          child: FlutterLogo()),
+                    const SizedBox(height: 16),
 
-                      // Başlık
-                      SelectableText(
-                        post.title,
-                        style: theme.textTheme.headlineSmall?.copyWith(
-                          fontWeight: FontWeight.bold,
+                    // Başlık
+                    SelectableText(
+                      post.title,
+                      style: theme.textTheme.headlineSmall?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+
+                    // Oluşturulma Tarihi
+                    Text(
+                      'Oluşturulma Tarihi: ${post.createdAt.toLocal().toString().split(' ')[0]}',
+                      style: theme.textTheme.bodySmall?.copyWith(
+                        color:
+                            isDarkMode ? Colors.white70 : Colors.grey.shade600,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+
+                    // İçerik
+                    SelectableLinkify(
+                      onOpen: (link) async {
+                        final Uri uri = Uri.parse(link.url);
+                        if (await canLaunchUrl(uri)) {
+                          await launchUrl(uri,
+                              mode: LaunchMode.externalApplication);
+                        } else {
+                          Get.snackbar('Hata', 'URL açılamadı.');
+                        }
+                      },
+                      text: post.content,
+                      style: GoogleFonts.openSans(
+                        textStyle: theme.textTheme.bodyLarge?.copyWith(
+                          fontSize: 18.0,
+                          height: 1.6,
+                          color: theme.textTheme.bodyLarge?.color,
                         ),
                       ),
-                      const SizedBox(height: 8),
-
-                      // Oluşturulma Tarihi
-                      Text(
-                        'Oluşturulma Tarihi: ${post.createdAt.toLocal().toString().split(' ')[0]}',
-                        style: theme.textTheme.bodySmall?.copyWith(
-                          color: isDarkMode
-                              ? Colors.white70
-                              : Colors.grey.shade600,
-                        ),
+                      linkStyle: TextStyle(
+                        color: theme.colorScheme.primary,
+                        decoration: TextDecoration.underline,
                       ),
-                      const SizedBox(height: 24),
-
-                      // İçerik
-                      SelectableLinkify(
-                        onOpen: (link) async {
-                          final Uri uri = Uri.parse(link.url);
-                          if (await canLaunchUrl(uri)) {
-                            await launchUrl(uri,
-                                mode: LaunchMode.externalApplication);
-                          } else {
-                            Get.snackbar('Hata', 'URL açılamadı.');
-                          }
-                        },
-                        text: post.content,
-                        style: GoogleFonts.openSans(
-                          textStyle: theme.textTheme.bodyLarge?.copyWith(
-                            fontSize: 18.0,
-                            height: 1.6,
-                            color: theme.textTheme.bodyLarge?.color,
-                          ),
-                        ),
-                        linkStyle: TextStyle(
-                          color: theme.colorScheme.primary,
-                          decoration: TextDecoration.underline,
-                        ),
-                        options: const LinkifyOptions(looseUrl: true),
-                        textAlign: TextAlign.left,
-                        maxLines: null,
-                      ),
-                    ],
-                  ),
+                      options: const LinkifyOptions(looseUrl: true),
+                      textAlign: TextAlign.left,
+                      maxLines: null,
+                    ),
+                  ],
                 ),
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      }),
     );
   }
 }
